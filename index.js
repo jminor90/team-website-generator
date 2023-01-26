542f751a8dd26553e3abd4f9b99e58a630e3281c
Joshua <120212106+jminor90@users.noreply.github.com>
2023-01-25 19:18:23 -0700

    Functions Mostly as Intended. Still needs cleaning up and final touches

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
+
 const questionType = [
   {
     message: 'Choose an employee type',
     type: 'list',
     choices: [
         'Engineer',
-        'Intern'
+        'Intern',
+        'None'
     ],
     name: 'employeeType'
   }
@@ -22,7 +24,7 @@ const questionType = [
 const managerQuestions = [
   {
     message: `Name of Manager: `,
-    type: `Input`,
+    type: `input`,
     name: 'managerName'
   },
   {
@@ -34,23 +36,310 @@ const managerQuestions = [
     message: `Manager Email: `,
     type: 'input',
     name: 'managerEmail'
+  },
+  {
+    message: `Manager Office #: `,
+    type: `input`,
+    name: `managerOffice`
   }
 ]
 
+const engineerQuestions = [
+  {
+    message: `Name of Engineer: `,
+    type: `input`,
+    name: `engineerName`,
+  },
+  {
+    message: `Engineer ID#: `,
+    type: `input`,
+    name: `engineerID`,
+  },
+  {
+    message: `Engineer Email`,
+    type: `input`,
+    name: `engineerEmail`
+  },
+  {
+    message: `Engineer GitHub: `,
+    input: `input`,
+    name: `engineerGithub`
+  }
 
+]
+
+const internQuestions = [
+  {
+    message: `Name of Intern: `,
+    type: `input`,
+    name: `internName`,
+  },
+  {
+    message: `Intern ID#: `,
+    type: `input`,
+    name: `internID`,
+  },
+  {
+    message: `Intern Email: `,
+    type: `input`,
+    name: `internEmail`
+  },
+  {
+    message: `Intern School: `,
+    input: `input`,
+    name: `internSchool`
+  }
+
+
+]
+
+const employeeArray = [];
+
+const htmlArray = [];
+
+
+/*
+Prompt Manager Questions first cause only one Manager
+Take manager answers and plug them into Manager.JS to make that class
+use the info plugged into Manager.JS to generate HTML
+
+Then Ask if next Employee is Intern, Engineer? 
+(Does Employee encompass all?)
+prompt questions for appropriate type
+Use answers for that to generate HTML in the appropriate format
+Rinse & Repeat
+
+Tool to keep track of created employees - Array
+use push method
+
+All employees will have employee class and use Extend to add on other classes (Manager, Intern, Engineer)
+*/
 function init() {
   inquirer
   .prompt(managerQuestions)
-  .then(answer => new Manager(answer))
+  .then(answer => {
+    new Manager(answer)
+    employeeArray.push(answer)
+    managerCard(answer)
+    console.info(employeeArray)
+    secondInit()
+    
+  }) // Tool to keep track of created employees, then ask further questions. (array)
+
   .catch(error => {
     if(error){
       throw error
     }
   })
 
-  //console.log("Nice one")
+}
 
-  //console.log(Employee)
+function managerCard(Manager) {
+  const managerHTML = [
+`
+<div class="card m-3 bg-info" style="width: 18rem;">
+<div div class="card-header">
+  ${Manager.managerName}
+</div>
+<ul class="list-group list-group-flush">
+  <li class="list-group-item">${Manager.managerID}</li>
+  <li class="list-group-item">${Manager.managerEmail}</li>
+  <li class="list-group-item">${Manager.managerOffice}</li>
+</ul>
+</div>
+`
+  ]
+
+  htmlArray.push(managerHTML)
+  //console.log(htmlArray)
+}
+
+function secondInit () {
+  
+  inquirer
+  .prompt(questionType)
+  .then(answer => {
+    handleInitialChoice(answer)}
+    )
+  .catch(console.error)
+}
+
+function handleInitialChoice (  answer ) {
+
+  switch (answer.employeeType) {
+    case 'Engineer':
+      createEngineer();
+      break;
+
+    case 'Intern':
+      createIntern();
+      break;
+    
+    default:
+      //console.log(answer)
+      console.log("Good Bye!")
+      generateHTML()
+      //process.exit()
+    
+    }
+  
+}
+
+
+function createEngineer() {
+  inquirer
+  .prompt(engineerQuestions)
+  .then(answer => {
+    new Engineer(answer)
+    employeeArray.push(answer)
+
+    engineerCard(answer)
+    console.info(employeeArray)
+    secondInit()
+  })
+    
+  .catch(console.error)
+  
+}
+
+function engineerCard(Engineer) {
+  const engineerHTML = [
+`
+<div class="card m-3 bg-info" style="width: 18rem;">
+<div div class="card-header">
+  ${Engineer.engineerName}
+</div>
+<ul class="list-group list-group-flush">
+  <li class="list-group-item">${Engineer.engineerID}</li>
+  <li class="list-group-item">${Engineer.engineerEmail}</li>
+  <li class="list-group-item">${Engineer.engineerGithub}</li>
+</ul>
+</div>
+`
+  ]
+
+  htmlArray.push(engineerHTML)
+  //console.log(htmlArray)
+}
+
+function createIntern() {
+  inquirer
+  .prompt(internQuestions)
+  .then(answer => {
+    new Intern(answer)
+    employeeArray.push(answer)
+
+    internCard(answer)
+    console.info(employeeArray)
+    secondInit()
+  })
+  .catch(console.error)
+}
+
+function internCard(Intern) {
+  const internHTML = [
+`
+<div class="card m-3 bg-info" style="width: 18rem;">
+<div div class="card-header">
+  ${Intern.internName}
+</div>
+<ul class="list-group list-group-flush">
+  <li class="list-group-item">${Intern.internID}</li>
+  <li class="list-group-item">${Intern.internEmail}</li>
+  <li class="list-group-item">${Intern.internSchool}</li>
+</ul>
+</div>
+`
+  ]
+
+  htmlArray.push(internHTML)
+  //console.log(htmlArray)
+}
+
+function generateHTML () {
+
+  console.log(htmlArray)
+
+  const yayHTML = 
+`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" 
+  rel="stylesheet" 
+  integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" 
+  crossorigin="anonymous">
+  <title>Team Website</title>
+</head>
+<body>
+  <div class="container-fluid">
+    <nav class="navbar navbar-expand-lg bg-success">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">TEAM TIME</a>
+      </div>
+    </nav>
+  </div>
+  <div class="container row">
+`
+  const footHTML = 
+`
+</div>
+  <footer>
+  Generated by jminor90
+  </footer>
+</body>
+</html>
+`
+
+/* Must figure out how to make the array concat to string
+let finalHTML = yayHTML + htmlArray.forEach(element => {
+  
+});
+
+console.log(finalHTML)
+*/
+
+let cardHTML = ``;
+
+for (let i = 0; i < htmlArray.length; i++) {
+  cardHTML = cardHTML.concat(htmlArray[i]);
+}
+
+const finalHTML = yayHTML.concat(cardHTML).concat(footHTML)
+
+//console.log(finalHTML);
+
+writeHTML(finalHTML)
+/*
+  fs.writeFile('index.HTML', finalHTML , (err) => {
+    if (err) {
+      throw err;
+    }
+
+    console.info("File has been written");
+    init();
+  })
+  */
+}
+
+function writeHTML(finalHTML) {
+  const html = finalHTML
+
+  console.log(html)
+
+  fs.writeFileSync('index.HTML', html, (err) => {
+    if (err) {
+      throw err;
+    }
+
+    console.info("File has been written");
+    init();
+  })
+  process.exit()
 }
 
 init();
+
